05440c6a4d837b2e9444245927acac64483e0e22
Laura DeCicco <ldecicco@usgs.gov>
2015-08-20 12:09:55 -0500

    Added more plots.

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -1,4 +1,37 @@
+library(EGRET)
+
+eList <- Choptank_eList
+
+
 shinyServer(function(input, output) {
   
+  output$flowPlotsOut <- renderPlot({ 
+    if(is.null(input$flowStat)){
+      stat=5
+    } else {
+      stat = as.integer(input$flowStat)
+    }
+    
+    switch(input$flowPlots,
+           "plotFlowSingle" = plotFlowSingle(eList, istat=stat),
+           "plotSDLogQ" = plotSDLogQ(eList),
+           "plotQTimeDaily" = plotQTimeDaily(eList),
+           "plotFour" = plotFour(eList),
+           "plotFourStats" = plotFourStats(eList)
+           
+           )
+  })
+  
+  output$dataPlotsOut <- renderPlot({ 
+    
+    switch(input$dataPlots,
+           "boxConcMonth" = boxConcMonth(eList),
+           "boxQTwice" = boxQTwice(eList),
+           "plotConcTime" = plotConcTime(eList),
+           "plotConcQ" = plotConcQ(eList),
+           "multiPlotDataOverview" = multiPlotDataOverview(eList)
+           
+    )
+  })
   
 })
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -4,37 +4,45 @@ eList <- Choptank_eList
 
 shinyUI(
   fluidPage(
-    
+    h2("EGRET Exploration"),
     fluidRow(
-      column(1),
-      column(10,
+      column(11,
              tabsetPanel(
                tabPanel("Flow History",
                         fluidRow(
                           column(4,
                               selectInput("flowPlots", label = "Flow History", 
                                  choices = c("plotFlowSingle","plotSDLogQ","plotQTimeDaily","plotFour","plotFourStats"),
-                                 selected = "plotFlowSingle", multiple = FALSE) 
+                                 selected = "plotFlowSingle", multiple = FALSE),
+                              selectInput("flowStat", label = "Flow Statistic", 
+                                          choices = list("1-day minimum"=1, "7-day minimum"=2, "30-day minimum"=3, "median"=4,
+                                                         "mean"=5, "30-day maximum"=6, "7-day maximum"=7, "1-day maximum"=8),
+                                          selected = 5, multiple = FALSE)
                                  
-                                 )
+                                 ),
+                          column(8,
+                                 plotOutput("flowPlotsOut"))
                         )),
                tabPanel("Explore Data",
                         fluidRow(
                           column(4,
-                             selectInput("data", label = "Data", 
-                                 choices = c("Water Sample",
-                                             "Passive Samples"),
-                                 selected = "Water Sample + ToxCast", multiple = FALSE) 
+                             selectInput("dataPlots", label = "Data", 
+                                 choices = c("boxConcMonth","boxQTwice","plotConcTime","plotConcQ","multiPlotDataOverview"),
+                                 selected = "multiPlotDataOverview", multiple = FALSE) 
                           
-                        )
+                        ),
+                        column(8,
+                               plotOutput("dataPlotsOut"))
                       )),
                tabPanel("Explore Model",
                         fluidRow(
                           column(4,
-                             selectInput("data", label = "Data", 
-                                 choices = c("Water Sample",
-                                             "Passive Samples"),
-                                 selected = "Water Sample + ToxCast", multiple = FALSE)  
+                             selectInput("modelPlots", label = "Data", 
+                                 choices = c("plotConcTimeDaily","plotFluxTimeDaily","plotConcPred","plotFluxPred","plotResidPred",
+                                             "plotResidQ","plotResidTime","boxResidMonth","boxConcThree",
+                                             "plotConcHist","plotFluxHist","plotConcQSmooth","plotConcTimeSmooth",
+                                             "fluxBiasMulti","plotContours","plotDiffContours"),
+                                 selected = "fluxBiasMulti", multiple = FALSE)  
                           
                         )
                     ))               
